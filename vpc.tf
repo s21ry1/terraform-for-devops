510f00aecdd47161d889c02bb6f342d2bde08f67
s21ry1 <surya7official@gmail.com>
2025-06-16 23:35:57 +0530

    vpc file

---
+++ vpc.tf
@@ -0,0 +1,119 @@
+# Terraform VPC Services Example
+# This file contains 10 different VPC-related resources, each as a separate resource block
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# 1. VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "MainVPC"
+  }
+}
+
+# 2. Subnet
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "PublicSubnet"
+  }
+}
+
+# 3. Subnet (Private)
+resource "aws_subnet" "private" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "PrivateSubnet"
+  }
+}
+
+# 4. Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "MainIGW"
+  }
+}
+
+# 5. NAT Gateway
+resource "aws_eip" "nat" {
+  vpc = true
+}
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+  tags = {
+    Name = "MainNAT"
+  }
+}
+
+# 6. Route Table (Public)
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+  tags = {
+    Name = "PublicRouteTable"
+  }
+}
+
+# 7. Route Table Association (Public)
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+# 8. Route Table (Private)
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+  tags = {
+    Name = "PrivateRouteTable"
+  }
+}
+
+# 9. Route Table Association (Private)
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+
+# 10. Network ACL
+resource "aws_network_acl" "main" {
+  vpc_id = aws_vpc.main.id
+  subnet_ids = [aws_subnet.public.id, aws_subnet.private.id]
+  egress {
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+    protocol   = "-1"
+  }
+  ingress {
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+    protocol   = "-1"
+  }
+  tags = {
+    Name = "MainACL"
+  }
+}
